83a9a0dd1a4b6a82e975728b55deca191d76d4b1
Marcos Wellington <marcoswoc@gmail.com>
2022-04-18 17:20:18 -0300

    Add DataContext

--- BlazorEcommerce/Server/Program.cs
+++ BlazorEcommerce/Server/Program.cs
@@ -1,9 +1,16 @@
 global using BlazorEcommerce.Shared;
+using BlazorEcommerce.Server.Data;
 using Microsoft.AspNetCore.ResponseCompression;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+builder.Services.AddDbContext<DataContext>(options => {
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+
 
 builder.Services.AddControllersWithViews();
 builder.Services.AddRazorPages();
