8731c0e4e25d470e9491e6d1cd4ab707172a42d3
Marcos Wellington <marcoswoc@gmail.com>
2022-04-20 15:00:58 -0300

    Create First Migration

--- BlazorEcommerce/Server/Program.cs
+++ BlazorEcommerce/Server/Program.cs
@@ -10,8 +10,6 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDbContext<DataContext>(options => {
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
-
-
 builder.Services.AddControllersWithViews();
 builder.Services.AddRazorPages();
 
