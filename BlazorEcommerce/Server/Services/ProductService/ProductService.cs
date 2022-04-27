a4201ce038ece5d6bb0951bb145b1982f8e9f5c6
Marcos Wellington <marcoswoc@gmail.com>
2022-04-27 20:44:39 -0300

    Display the Price in the ProductList Component

--- BlazorEcommerce/Server/Services/ProductService/ProductService.cs
+++ BlazorEcommerce/Server/Services/ProductService/ProductService.cs
@@ -14,7 +14,11 @@ public class ProductService : IProductService
     public async Task<ServiceResponse<Product>> GetProductAsync(int productId)
     {
         var response = new ServiceResponse<Product>();
-        var product = await _context.Products.FindAsync(productId);
+        var product = await _context.Products
+            .Include(x => x.Variants)
+            .ThenInclude(v => v.ProductType)
+            .FirstOrDefaultAsync(p => p.Id == productId);
+
         if (product is null)
         {
             response.Success = false;
@@ -33,7 +37,9 @@ public class ProductService : IProductService
     {
         var response = new ServiceResponse<List<Product>>()
         {
-            Data = await _context.Products.ToListAsync()
+            Data = await _context.Products
+            .Include(p => p.Variants)
+            .ToListAsync()
         };
 
         return response;
@@ -45,6 +51,7 @@ public class ProductService : IProductService
         {
             Data = await _context.Products
             .Where(p => p.Category.Url.ToLower().Equals(categoryUrl.ToLower()))
+            .Include(p => p.Variants)
             .ToListAsync()
         };
 
