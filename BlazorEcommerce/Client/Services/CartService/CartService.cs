dc70e9cc1c3877a0200ba722805b988bb58107a9
Marcos Wellington <marcoswoc@gmail.com>
2022-05-12 22:32:31 -0300

    Implement a cartService on the client

---
+++ BlazorEcommerce/Client/Services/CartService/CartService.cs
@@ -0,0 +1,37 @@
+﻿using BlazorEcommerce.Shared;
+using Blazored.LocalStorage;
+
+namespace BlazorEcommerce.Client.Services.CartService;
+
+public class CartService : ICartService
+{
+    private readonly ILocalStorageService _localStorageService;
+
+    public CartService(ILocalStorageService localStorageService)
+    {
+        _localStorageService = localStorageService;
+    }
+
+    public event Action OnChage;
+
+
+    public async Task AddToCart(CartItem cartItem)
+    {
+        var cart = await _localStorageService.GetItemAsync<List<CartItem>>("cart");
+
+        if (cart is null) cart = new List<CartItem>();
+
+        cart.Add(cartItem);
+
+        await _localStorageService.SetItemAsync("cart", cart);
+    }
+
+    public async Task<List<CartItem>> GetCartItems()
+    {
+        var cart = await _localStorageService.GetItemAsync<List<CartItem>>("cart");
+
+        if (cart is null) cart = new List<CartItem>();
+
+        return cart;
+    }
+}
---
+++ BlazorEcommerce/Client/Services/CartService/ICartService.cs
@@ -0,0 +1,10 @@
+﻿using BlazorEcommerce.Shared;
+
+namespace BlazorEcommerce.Client.Services.CartService;
+
+public interface ICartService
+{
+    event Action OnChage;
+    Task AddToCart(CartItem cartItem);
+    Task<List<CartItem>> GetCartItems();
+}
