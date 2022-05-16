07d1e49866dfb66ceaecc48c267c3858ab708368
Marcos Wellington <marcoswoc@gmail.com>
2022-05-16 19:04:12 -0300

    Add the UserRegister model

---
+++ BlazorEcommerce/Shared/UserRegister.cs
@@ -0,0 +1,7 @@
+﻿namespace BlazorEcommerce.Shared;
+public class UserRegister
+{
+    public string Email { get; set; } = string.Empty;
+    public string Password { get; set; } = string.Empty;
+    public string ConfirmPassword { get; set; } = string.Empty;
+}
